5a2454f2d88da9b3d3b8cdf61a360af7abe77108
Tauri <Tauritammela272@gmail.com>
2025-11-23 14:15:18 +0200

    rewrote dashboard in daisyui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <h2 class="text-2xl font-bold">{{ __('Dashboard') }}</h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+    <div class="max-w-md mx-auto my-6">
+        <div class="card bg-base-100 shadow">
+            <div class="card-body">
+                <p>{{ ("You're logged in!") }}</p>
             </div>
         </div>
     </div>
